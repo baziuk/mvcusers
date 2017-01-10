40d97effd54fd541106465b27640344ec0799a39
baziuk <bazuk18@gmail.com>
2017-01-10 04:15:13 +0200

    Update users.sql

--- www/users.sql
+++ www/users.sql
@@ -10,7 +10,7 @@ primary key(`id`)
 CREATE TABLE IF NOT EXISTS users (
 `id` int(10) unsigned NOT NULL auto_increment,
 `user_name` varchar(255),
-`user_mail` varchar(255),
+`user_email` varchar(255),
 `user_country_id` int(10) unsigned, 
 primary key (`id`),
 foreign key (`user_country_id`) references countries(`id`) ON
